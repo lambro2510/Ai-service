69e5d0d9baa1e996840c21f45d8cab3c3b6cdd9c
lambro25102001@gmail.com <lambro25102001@gmail.com>
2023-08-23 17:54:15 +0630

    init

--- src/main/java/com/lambro2510/service/controller/LanguageDataTrainingController.java
+++ src/main/java/com/lambro2510/service/controller/LanguageDataTrainingController.java
@@ -6,13 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequestMapping("api/v1/ai/language")
 @RestController
 public class LanguageDataTrainingController {
 
   @Autowired private DataTrainingService dataTrainingService;
   @PostMapping("")
-  public ResponseEntity<Boolean> createData(@RequestBody CreateLanguageDataTrainingDto dto){
+  public ResponseEntity<Boolean> createData(@RequestBody List<CreateLanguageDataTrainingDto> dto){
     dataTrainingService.createLanguageDataTraining(dto);
     return ResponseEntity.ok(true);
   }
--- src/main/java/com/lambro2510/service/service/DataTrainingService.java
+++ src/main/java/com/lambro2510/service/service/DataTrainingService.java
@@ -5,12 +5,16 @@ import com.lambro2510.service.entity.LanguageDataTraining;
 import com.lambro2510.service.entity.types.TextStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DataTrainingService extends BaseService{
   
-  public void createLanguageDataTraining(CreateLanguageDataTrainingDto dto){
-    LanguageDataTraining dataTraining = createData(dto.getText(), dto.getStatus(), dto.getPercent());
-    languageDataTrainingRepository.save(dataTraining);
+  public void createLanguageDataTraining(List<CreateLanguageDataTrainingDto> dto){
+    for(CreateLanguageDataTrainingDto trainingDto : dto){
+      LanguageDataTraining dataTraining = createData(trainingDto.getText(), trainingDto.getStatus(), trainingDto.getPercent());
+      languageDataTrainingRepository.save(dataTraining);
+    }
   }
 
   private LanguageDataTraining createData(String text, TextStatus status, Integer percent){
