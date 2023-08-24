5f5733a75e9d1a96d29666eef64390bafa0fe866
lambro25102001@gmail.com <lambro25102001@gmail.com>
2023-08-24 16:01:11 +0630

    init

--- src/main/java/com/lambro2510/service/Utils/Helper.java
+++ src/main/java/com/lambro2510/service/Utils/Helper.java
@@ -4,26 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Helper {
-  public static boolean isMaxValueDoubled(double[] outcomes, Double percent) {
+  public static boolean isMaxValueOutcome(double[] outcomes, Double percent) {
+    percent = 1 / percent;
     if (outcomes == null || outcomes.length == 0) {
       return false;
     }
 
-    double max = Double.NEGATIVE_INFINITY; // Sử dụng Double.NEGATIVE_INFINITY để đảm bảo rằng max có giá trị thấp nhất ban đầu
+    for (int value = 0 ; value < outcomes.length; value ++) {
+      if(value == 0 || value == 1 || value == 2){
+        if(outcomes[value] > outcomes[3] * percent || outcomes[value] > outcomes[4] * percent || outcomes[value] > outcomes[5] * percent || outcomes[value] > outcomes[6] * percent){
+          return true;
+        }
+      }
 
-    for (double value : outcomes) {
-      if (value > max) {
-        max = value;
+      if(value == 3 || value == 4){
+        if(outcomes[value] > outcomes[0] * percent || outcomes[value] > outcomes[1] * percent || outcomes[value] > outcomes[2] * percent || outcomes[value] > outcomes[5] * percent || outcomes[value] > outcomes[6] * percent){
+          return true;
+        }
+      }
+
+      if(value == 5 || value == 6){
+        if(outcomes[value] > outcomes[0] * percent || outcomes[value] > outcomes[1] * percent || outcomes[value] > outcomes[2] * percent || outcomes[value] > outcomes[3] * percent || outcomes[value] > outcomes[4] * percent){
+          return true;
+        }
       }
     }
 
-    for (double value : outcomes) {
-      if (value != max && value <= max * percent) { // Kiểm tra xem giá trị có lớn hơn hoặc bằng 70% giá trị lớn nhất hay không
-        return false;
-      }
-    }
-
-    return true;
+    return false;
   }
 
   public static List<String> splitTextIntoSentences(String text) {
--- src/main/java/com/lambro2510/service/factory/LanguageAiComponent.java
+++ src/main/java/com/lambro2510/service/factory/LanguageAiComponent.java
@@ -85,7 +85,7 @@ public class LanguageAiComponent {
     }
     log.info("Outcome --> inputData --> : {{}}", outcomes);
     textStatus = Arrays.stream(TextStatus.values()).filter(data -> data.toString().equals(status)).toList().get(0);
-    return new LanguageDataResponse(textStatus,textStatus.getDescription(), percent, Helper.isMaxValueDoubled(outcomes, 0.9));
+    return new LanguageDataResponse(textStatus,textStatus.getDescription(), percent, Helper.isMaxValueOutcome(outcomes, 0.5));
   }
 
 }
